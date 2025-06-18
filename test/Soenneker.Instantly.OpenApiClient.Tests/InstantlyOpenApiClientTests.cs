c530800f46a5aaa539ead4cb4c45b34ea77d8552
Jake Soenneker <jake@soenneker.com>
2025-06-17 19:42:15 -0500

    Initial commit

---
+++ test/Soenneker.Instantly.OpenApiClient.Tests/InstantlyOpenApiClientTests.cs
@@ -0,0 +1,22 @@
+﻿using Soenneker.Instantly.OpenApiClient.Abstract;
+using Soenneker.Tests.FixturedUnit;
+using Xunit;
+
+namespace Soenneker.Instantly.OpenApiClient.Tests;
+
+[Collection("Collection")]
+public sealed class InstantlyOpenApiClientTests : FixturedUnitTest
+{
+    private readonly IInstantlyOpenApiClient _kiota;
+
+    public InstantlyOpenApiClientTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    {
+        _kiota = Resolve<IInstantlyOpenApiClient>(true);
+    }
+
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
