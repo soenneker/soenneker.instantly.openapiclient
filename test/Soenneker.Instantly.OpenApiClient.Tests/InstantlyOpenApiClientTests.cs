b19286ca4ee42d901bf4465ef0622b4cf407d4de
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:03:19 -0500

    TUnit conversion

--- test/Soenneker.Instantly.OpenApiClient.Tests/InstantlyOpenApiClientTests.cs
+++ test/Soenneker.Instantly.OpenApiClient.Tests/InstantlyOpenApiClientTests.cs
@@ -1,16 +1,15 @@
-﻿using Soenneker.Tests.FixturedUnit;
-using Xunit;
+using Soenneker.Tests.HostedUnit;
 
 namespace Soenneker.Instantly.OpenApiClient.Tests;
 
-[Collection("Collection")]
-public sealed class InstantlyOpenApiClientTests : FixturedUnitTest
+[ClassDataSource<Host>(Shared = SharedType.PerTestSession)]
+public sealed class InstantlyOpenApiClientTests : HostedUnitTest
 {
-    public InstantlyOpenApiClientTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    public InstantlyOpenApiClientTests(Host host) : base(host)
     {
     }
 
-    [Fact]
+    [Test]
     public void Default()
     {
 
